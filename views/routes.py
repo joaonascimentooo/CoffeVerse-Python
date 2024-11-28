ae964110541f4d14f989b91177d66965da1975df
joaonascimentooo <joaonascimento197@gmail.com>
2024-11-28 17:12:57 -0300

    feature/add avaliation

--- views/routes.py
+++ views/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from db import get_db_connection  
-#Empresas
+
+# Empresas
 def register_routes(app):
     @app.route('/empresas')
     def index():
@@ -12,11 +13,12 @@ def register_routes(app):
         connection.close()
 
         return render_template('empresas.html', empresas=empresas)
-    
+
+    # Página inicial
     @app.route('/')
     def home():
         return render_template('home.html')
-    
+
     @app.route('/empresa/<int:empresa_id>')
     def empresa_detalhes(empresa_id):
         connection = get_db_connection()
@@ -24,10 +26,9 @@ def register_routes(app):
 
         cursor.execute("SELECT * FROM tb_empresa WHERE id = %s", (empresa_id,))
         empresa = cursor.fetchone()
-        
+
         cursor.execute("SELECT * FROM tb_produto WHERE empresa_id = %s", (empresa_id,))
         produtos = cursor.fetchall()
-        
         cursor.close()
         connection.close()
 
@@ -35,7 +36,7 @@ def register_routes(app):
             return "Empresa não encontrada", 404
 
         return render_template('empresa_detalhes.html', empresa=empresa, produtos=produtos)
-    
+
     @app.route('/compra/<int:produto_id>', methods=['GET'])
     def compra(produto_id):
         connection = get_db_connection()
@@ -48,8 +49,8 @@ def register_routes(app):
         if produto is None:
             return "Produto não encontrado", 404  
 
-        return render_template('compra.html', produto=produto)  
-    
+        return render_template('compra.html', produto=produto)
+
     @app.route('/finalizar_compra/<int:produto_id>', methods=['POST'])
     def finalizar_compra(produto_id):
         quantidade = request.form['quantidade']
@@ -67,10 +68,46 @@ def register_routes(app):
 
         return render_template('compra_finalizada.html', produto=produto, quantidade=quantidade, total=total)
 
-    
-    
+    @app.route('/avaliar_compra/<int:produto_id>', methods=['GET', 'POST'])
+    def avaliar_compra(produto_id):
+        if request.method == 'POST':
+            avaliacao = request.form['avaliacao']
+            comentario = request.form['comentario']
+            cliente_id = 1  
 
+            connection = get_db_connection()
+            cursor = connection.cursor()
+            cursor.execute("""
+                INSERT INTO tb_avaliacao (empresa_id, cliente_id, avaliacao, comentario)
+                SELECT empresa_id, %s, %s, %s
+                FROM tb_produto
+                WHERE id = %s
+            """, (cliente_id, avaliacao, comentario, produto_id))
+            connection.commit()
+            cursor.close()
+            connection.close()
 
+            connection = get_db_connection()
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT empresa_id FROM tb_produto WHERE id = %s", (produto_id,))
+            empresa = cursor.fetchone()
+            cursor.close()
+            connection.close()
 
+            if empresa:
+                return redirect(url_for('empresa_detalhes', empresa_id=empresa['empresa_id']))
+            else:
+                return "Empresa não encontrada", 404
 
+        connection = get_db_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM tb_produto WHERE id = %s", (produto_id,))
+        produto = cursor.fetchone()
+        cursor.close()
+        connection.close()
+
+        if produto is None:
+         return "Produto não encontrado", 404  
+
+        return render_template('avaliar_compra.html', produto=produto)
 
